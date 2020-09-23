f48871403fc1e27e97a9c1f900e980ff326175aa
熊涛 <xiongtao04@meituan.com>
2020-09-23 21:12:19 +0800

    2020-09-23 night

--- 动态规划/084_LargestRectangleArea.py
+++ 动态规划/084_LargestRectangleArea.py
@@ -14,5 +14,35 @@
 输出: 10
 '''
 
+'''
+题解：
+遍历整个数组heights，计算以heights[i]为右边界的最大面积。
+
+暴力方法：
+    1. 对于每个heights[i]，从i开始遍历i之前的元素j，计算[j,i]之间的面积（最小高度*宽度）；
+    2. 保留每次计算的最大面积。时间复杂度O(n^2)
+修改暴力方法：
+    1. 我们没有必要对每个heights[i]都计算一次，只需要对每次递增的峰值进行计算即可（峰值就是比右边元素大的值）；
+    2. 原因是因为，若元素a[i]是峰值，即a[i] >= a[i-1]，那么以a[i]为右边界的最大面积 >= 以a[i-1]为右边界的最大面积；
+
+'''
+
 class LargestRectangleArea:
+    # 修改的暴力方法
     def largestRectangleArea(self, heights):
+        max_Area = 0
+        for i in range(len(heights)):
+            # 确定是否是峰值
+            if (i < len(heights) - 1 and heights[i] > heights[i + 1]) or i == len(heights) - 1:
+                # 计算以i为右边界的最大面积
+                min_num = heights[i]
+                for j in range(i, -1, -1):
+                    # 计算[j,i]之间的最小值
+                    min_num = min(min_num, heights[j])
+                    max_Area = max(max_Area, min_num * (i - j + 1))
+        return max_Area
+
+if __name__ == '__main__':
+    class_LRA = LargestRectangleArea()
+    heights = [2,1,5,6,2,3]
+    print(class_LRA.largestRectangleArea(heights))
