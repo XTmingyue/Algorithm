de5cb00d150675622e4062fb2930cd6931c028c8
熊涛 <xiongtao@xiongtaodeMacBook-Pro.local>
2020-10-19 09:16:30 +0800

    2020-10-19 morning

--- 动态规划/140_WordBreak_II.py
+++ 动态规划/140_WordBreak_II.py
@@ -37,4 +37,32 @@ wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
 '''
 
 class WordBreak:
+    '''
+    题解：动态规划
+    1. 沿用139题中的转移数组dp[i]：表示s[0:i+1]是否可以被拆分为wordDict数组中的元素组合
+    2. dp[len(s)-1] == False时，说明s不能被拆分为wordDict中的元素组合，那么直接返回 []
+    3. dp[len(s)-1] == True时，我们采用递归的方式求解所有可能的组合：
+        a. 从右到左遍历dp数组中的元素k，当s[k+1:] in wordDict中时，递归确定s[0:k+1]的所有组合；
+        b. 注意可能存在多个k满足 s[k+1:] in wordDict中，对于每一种都要递归的确定s[0:k+1]的所有组合；
+    '''
     def wordBreak(self, s, wordDict):
+        # 首先计算dp数组
+        n = len(s)
+        if n == 0:
+            return []
+        dp = [False] * n
+        for i in range(0, n):
+            if s[0:i+1] in wordDict:
+                dp[i] = True
+            else:
+                for j in range(1, i):
+                    if s[j:i+1] in wordDict:
+                        dp[i] = dp[i] or dp[j-1]
+
+        return dp[-1]
+
+if __name__ == '__main__':
+    WB = WordBreak()
+    s = "applepenapple"
+    wordDict = ["apple", "pen"]
+    print(WB.wordBreak(s, wordDict))
